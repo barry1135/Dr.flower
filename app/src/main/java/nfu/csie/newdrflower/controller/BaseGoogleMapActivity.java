684c04840c20fc10320537fe649ffdaf9d172ec1
barry1135 <barry159357@gmail.com>
2017-08-02 13:43:40 +0800

    2017.08.02 嘗試匯入library

---
+++ app/src/main/java/nfu/csie/newdrflower/controller/BaseGoogleMapActivity.java
@@ -0,0 +1,57 @@
+package nfu.csie.newdrflower.controller;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import nfu.csie.newdrflower.R;
+
+/**
+ * Created by barry on 2017/8/2.
+ */
+
+public abstract class BaseGoogleMapActivity extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+
+    protected int getLayoutId() {
+        return R.layout.googlemap;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutId());
+        setUpMap();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMap();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        if (mMap != null) {
+            return;
+        }
+        mMap = map;
+        startDemo();
+    }
+
+    private void setUpMap() {
+        ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.googlemap)).getMapAsync(this);
+    }
+
+    /**
+     * Run the demo-specific code.
+     */
+    protected abstract void startDemo();
+
+    protected GoogleMap getMap() {
+        return mMap;
+    }
+}
---
+++ app/src/main/java/nfu/csie/newdrflower/model/FlowerMap.java
@@ -0,0 +1,8 @@
+package nfu.csie.newdrflower.model;
+
+/**
+ * Created by barry on 2017/8/2.
+ */
+
+public class FlowerMap {
+}
